2ce7f6781503df446242bd75c4d221bfb663cb6a
Burak TURAN <burak.turan@outlook.com>
2022-06-23 00:54:09 +0300

    metini ekrandan alacak şekilde değiştirildi

--- SentimentAnalysis/analysis/views.py
+++ SentimentAnalysis/analysis/views.py
@@ -6,11 +6,14 @@ import os
 from django.conf import settings
 import string
 from django.http import HttpResponseBadRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 class analysisView(TemplateView):
     template_name = 'analysis/index.html'
+    @csrf_exempt
     def get_data(request, format=None):
         folder_path = os.path.join(settings.BASE_DIR, 'analysis')
-        text = open(folder_path+"\\metin.txt", encoding="utf-8").read()
+        #text = open(folder_path+"\\metin.txt", encoding="utf-8").read()
+        text=request.POST.get('text')
         lower_case = text.lower()
         cleaned_text = lower_case.translate(str.maketrans('', '', string.punctuation))
         tokenized_words = cleaned_text.split()
